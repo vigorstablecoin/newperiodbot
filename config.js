5e7998d0231ce3d3be8553bd6ccbb67b45bcf757
gg <andrew.peter.bryan@gmail.com>
2019-11-15 14:51:54 -0600

    initial commit

---
+++ config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    private_key: '5.....',
+    endpoint: 'http://api.eosnewyork.io',
+    contract: 'daccustodia1',
+    auth_user: 'stablequan11',
+    auth_perm: 'newperiod',
+    bot_apikey: '8...',
+    bot_apiurl: 'https://api.telegram.org/bot',
+    bot2_apikey: '8....'
+}
